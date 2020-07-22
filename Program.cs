669543c9e5ea423524db004336fa0df3ae88ecf9
Aren Maghakyan <alenmage@gmail.com>
2020-07-22 15:56:33 +0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Sorted_MultiMap
+{
+    class Program
+    {
+        static int Main(string[] args)
+        {
+
+            var mult = new SortedMultiMap<string, int>();
+
+            mult.Add("a", 1);
+            mult.Add("b", 2);
+            mult.Add("b", 3);
+            mult.Add("c", 4);
+
+
+            return 0;
+        }
+    }
+}
---
+++ SortedMultiMap.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Sorted_MultiMap
+{
+    class SortedMultiMap<K,V>
+    {
+        private readonly Dictionary<K, List<V>> _myDictionary = new Dictionary<K, List<V>>();
+
+        public void Add(K key, V value)
+        {
+            var hasKey = this._myDictionary.TryGetValue(key, out var list);
+
+            if (hasKey)
+            {
+                list.Add(value);
+                return;
+            }
+
+            var newList = new List<V> {value};
+            this._myDictionary[key] = newList;
+
+        }
+
+
+        public void Get(K key)
+        {
+            
+        }
+
+        public bool Remove(K key, V value)
+        { 
+            var hasKey = this._myDictionary.TryGetValue(key, out var valueList);
+
+            if (hasKey)
+            {
+                valueList.Remove(value);
+                return true;
+            }
+
+            return false;
+        }
+
+        public bool RemoveAll(K key)
+        {
+            return this._myDictionary.Remove(key);
+        }
+    }
+}
